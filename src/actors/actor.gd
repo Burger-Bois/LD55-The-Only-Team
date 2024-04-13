3ed0e25358405491d951af4539cbd72eb261d8d2
John Stones <john.stones@protonmail.com>
2024-04-13 17:16:19 +0100

    Add player click movement

--- src/actors/actor.gd
+++ src/actors/actor.gd
@@ -2,9 +2,20 @@ class_name Actor extends Node2D
 
 
 @export
-var coords: Vector2i
+var _coords: Vector2i
+
+var _level: Level
 
 
 func _enter_tree():
-    var level = get_parent() as Level
-    level.add_actor(coords, self)
+    _level = get_parent() as Level
+    _level.upsert_actor(self)
+
+
+func get_coords() -> Vector2i:
+    return _coords
+
+
+func set_coords(coords: Vector2i):
+    _coords = coords
+    _level.upsert_actor(self)
--- src/levels/level.gd
+++ src/levels/level.gd
@@ -3,6 +3,11 @@ class_name Level extends Node2D
 
 const DEFAULT_CELL_VALUE := null
 
+# Temporary
+@onready var player := $Actor as Actor
+
+var _actor_coords: Dictionary
+
 # Array[Array[Actor]]
 var _grid: Array[Array]
 
@@ -16,7 +21,32 @@ func _init():
 
 
 func _ready():
-    # Set actor positions based on coordinates
+    update_actor_positions()
+
+
+func _input(event: InputEvent):
+    if event is InputEventMouseButton \
+            and event.button_index == MOUSE_BUTTON_LEFT \
+            and event.pressed:
+        player.set_coords(mouse_coordinates())
+        update_actor_positions()
+
+
+func upsert_actor(actor: Actor):
+    if _actor_coords.has(actor):
+        # Remove actor from old coordinates
+        var old_coords := _actor_coords[actor] as Vector2i
+        _grid[old_coords.x][old_coords.y] = null
+
+    _grid[actor.get_coords().x][actor.get_coords().y] = actor
+    _actor_coords[actor] = actor.get_coords()
+
+
+func mouse_coordinates() -> Vector2i:
+    return floor(Vector2i(get_local_mouse_position()) / Constants.CELL_SIZE)
+
+
+func update_actor_positions():
     for column_i in range(_grid.size()):
         var column = _grid[column_i]
         for row_i in range(column.size()):
@@ -25,7 +55,3 @@ func _ready():
                 actor.position = \
                         Vector2(column_i, row_i) * Vector2(Constants.CELL_SIZE) \
                         + Vector2(Constants.CELL_SIZE) / 2
-
-
-func add_actor(coords: Vector2i, actor: Actor):
-    _grid[coords.x][coords.y] = actor
